66f386c8c27af23ea8d04de4326ab62d2b1eb685
Bart Jeukendrup <bart@jeukendrup.nl>
2015-04-29 22:07:30 +0200

    changed date format to bypass bug in addthisevent

--- views/default/event_manager/event/addthisevent.php
+++ views/default/event_manager/event/addthisevent.php
@@ -15,11 +15,11 @@ if (empty($location)) {
 $start = date(EVENT_MANAGER_FORMAT_DATE_EVENTDAY, $event->start_day) . " " . date('H', $event->start_time) . ":" . date('i', $event->start_time) . ":00";
 
 if ($event->end_ts) {
-	$end = date('Y-m-d H:i:00', $event->end_ts);
+	$end = date('d-m-Y H:i:00', $event->end_ts);
 } else {
 	$end_ts = mktime(date('H', $event->start_time), date('i', $event->start_time), 0,date('m', $event->start_day), date('d', $event->start_day), date('Y', $event->start_day));
 	$end_ts = $end_ts + 3600;
-	$end = date('Y-m-d H:i:00', $end_ts);
+	$end = date('d-m-Y H:i:00', $end_ts);
 }
 
 $title = $event->title;
@@ -38,24 +38,6 @@ $organizer = $event->organizer;
 		<span class="location"><?php echo $location; ?></span>
 		<span class="organizer"><?php echo $organizer;?></span>
 		<span class="organizer_email">noreply</span>
-		<span class="date_format">YYYY-MM-DD</span>
+		<span class="date_format">DD-MM-YYYY</span>
 	</div>
 </span>
-
-<?php
-
-/*
-
-<span class="_start">10-05-2012 11:38:46</span>
-<span class="_end">11-05-2012 11:38:46</span>
-<span class="_zonecode">35</span>
-<span class="_summary">Summary of the event</span>
-<span class="_description">Description of the event</span>
-<span class="_location">Location of the event</span>
-<span class="_organizer">Organizer</span>
-<span class="_organizer_email">Organizer e-mail</span>
-<span class="_facebook_event">http://www.facebook.com/events/160427380695693</span>
-<span class="_all_day_event">true</span>
-<span class="_date_format">DD/MM/YYYY</span>
-
- */
