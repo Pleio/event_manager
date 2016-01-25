e3339340784c398f98e0a9bb2ce53be39100a9ce
Bart Jeukendrup <bart@jeukendrup.nl>
2016-01-25 19:12:26 +0100

    fixed: moving directly to participants

--- actions/attendees/move_to_attendees.php
+++ actions/attendees/move_to_attendees.php
@@ -14,7 +14,16 @@ if (!empty($guid) && !empty($user)) {
 		if ($event->canEdit()) {
 			// check if object has relation ship that can be moved
 			$user_relationship = $event->getRelationshipByUser($object->getGUID());
-			if (in_array($user_relationship, array(EVENT_MANAGER_RELATION_ATTENDING_WAITINGLIST))) {
+			if (in_array($user_relationship, array(EVENT_MANAGER_RELATION_ATTENDING_PENDING, EVENT_MANAGER_RELATION_ATTENDING_WAITINGLIST))) {
+
+				// update pending slots
+				$slots = $event->getRegisteredSlotsForEntity($object->getGUID(), EVENT_MANAGER_RELATION_SLOT_REGISTRATION_PENDING);
+				if ($slots) {
+					foreach($slots as $slot) {
+						$object->removeRelationship($slot->getGUID(), EVENT_MANAGER_RELATION_SLOT_REGISTRATION_PENDING);
+						$object->addRelationship($slot->getGUID(), EVENT_MANAGER_RELATION_SLOT_REGISTRATION_WAITINGLIST);
+					}
+				}
 
 				// update waiting slots
 				$slots = $event->getRegisteredSlotsForEntity($object->getGUID(), EVENT_MANAGER_RELATION_SLOT_REGISTRATION_WAITINGLIST);
