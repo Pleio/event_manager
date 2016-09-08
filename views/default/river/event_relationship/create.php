acc3a50801d9a092ba20488c12cf40086fe3b4ab
Bart Jeukendrup <bart@jeukendrup.nl>
2016-09-08 15:24:42 +0200

    fix bug when event is removed

--- views/default/river/event_relationship/create.php
+++ views/default/river/event_relationship/create.php
@@ -6,10 +6,11 @@
 	$subject_url = "<a href=\"{$user->getURL()}\">{$user->name}</a>";
 	$event_url = "<a href=\"" . $event->getURL() . "\">" . $event->title . "</a>";
 	
-	$relationtype = $event->getRelationshipByUser($user->getGUID()); 
-	
-	$string = elgg_echo("event_manager:river:event_relationship:create:" . $relationtype, array($subject_url, $event_url));
-	
+	if ($event instanceof Event) {
+		$relationtype = $event->getRelationshipByUser($user->getGUID());
+		$string = elgg_echo("event_manager:river:event_relationship:create:" . $relationtype, array($subject_url, $event_url));
+	}
+
 	echo elgg_view('river/item', array(
 		'item' => $vars['item'],
 		"summary" => $string
